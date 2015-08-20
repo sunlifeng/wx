c6eada9b5e558195bb0556b1b4c378f242d14f81
Glauco Junior <glaucojunior22@gmail.com>
2015-08-20 10:02:43 -0300

    Adiciona verificação de Sistema Operacional para rodar em várias plataformas

--- run_locally.sh
+++ run_locally.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e # exit if any command fails
 [ -d virtenv ] || virtualenv virtenv # create a virtual env to install new modules
+if [ -n "$COMSPEC" -a -x "$COMSPEC" ]
+then
+    # Running Windows
+    source virtenv/scripts/activate
+else
+    # Running Linux or MacOS
+    source virtenv/bin/activate
+fi
 source virtenv/bin/activate # activate your virtual env
 python setup.py install # install dependencies into virtual env, needed by web2py
 export PYTHONPATH=$PWD/libs/ # where web2py should look for gluon
