9b11ca84bacb632487fbf227bbd9af425220509a
Glauco Junior <glaucojunior22@gmail.com>
2015-08-20 10:45:09 -0300

    Corrige o script run_locally.sh

--- run_locally.sh
+++ run_locally.sh
@@ -4,12 +4,11 @@ set -e # exit if any command fails
 if [ -n "$COMSPEC" -a -x "$COMSPEC" ]
 then
     # Running Windows
-    source virtenv/scripts/activate
+    source virtenv/scripts/activate # activate your virtual env
 else
     # Running Linux or MacOS
-    source virtenv/bin/activate
+    source virtenv/bin/activate # activate your virtual env
 fi
-source virtenv/bin/activate # activate your virtual env
 python setup.py install # install dependencies into virtual env, needed by web2py
 export PYTHONPATH=$PWD/libs/ # where web2py should look for gluon
 python wsgi/web2py/web2py.py # start web2py
